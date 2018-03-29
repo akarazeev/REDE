834ce0f705ce903c6ed130fdceeacb6567331c4b
Anton Karazeev <anton.karazeev@gmail.com>
2018-03-29 11:34:56 +0300

    Move preproc function to prepare_dataset.py

--- utils/prepare_dataset.py
+++ utils/prepare_dataset.py
@@ -26,7 +26,7 @@ def preproc(freqs, modes):
 
     # Corrected here // AL 20.03
     m1 = round(min(modes))
-    m2 = round(min(modes))
+    m2 = round(max(modes))
     m_int = np.arange(m1, m2 + 1)
     tck = interpolate.CubicSpline(modes, freqs)
     omega_int = tck(m_int)
@@ -45,7 +45,7 @@ def preproc(freqs, modes):
 
     # Corrected here // AL 20.03
     ind_int = np.argmin(abs(T0 - omega_int))
-    ind = np.argmin(abs(m_total - m_int[ind_int]))
+    ind = np.argmin(abs(modes_total - m_int[ind_int]))
     #####
 
     w0 = omega_total[ind]
@@ -58,71 +58,81 @@ def preproc(freqs, modes):
     delta_omega_total = delta - (np.sign(delta - (D1 / 2)) * D1 * (abs(delta) > (D1 / 2)))
     lambda_grid = c / omega_total
 
+    # Get rid off data when Dint is more than 200GHz
+    a = ((omega_total < 282e12) & (abs(delta_omega_total) > 200e9))
+
+    if omega_total[a].shape[0] != 0:
+        get_rid_ind = np.argmin(abs(max(omega_total[a]) - omega_total))
+        omega_total = omega_total[get_rid_ind + 1:]
+        delta_omega_total = delta_omega_total[get_rid_ind + 1:]
+    #####
+
     return omega_total, delta_omega_total, D1_total, D2_total
 
 
-# Load data.
-mat = spio.loadmat('matlab_data/full_set.mat', squeeze_me=True)
+if __name__ == '__main__':
+    # Load data.
+    mat = spio.loadmat('matlab_data/full_set.mat', squeeze_me=True)
 
-struct = mat['str']
-header = ['id']
-header.extend(struct[0][1].dtype.names)
-header
+    struct = mat['str']
+    header = ['id']
+    header.extend(struct[0][1].dtype.names)
+    header
 
-# Create DataFrame.
-dataset = []
+    # Create DataFrame.
+    dataset = []
 
-for i in range(len(struct)):
-    tmp = [int(struct[i][0])]
-    tmp.extend([float(struct[i][1][name]) for name in header[1:]])
-    dataset.append(tmp)
+    for i in range(len(struct)):
+        tmp = [int(struct[i][0])]
+        tmp.extend([float(struct[i][1][name]) for name in header[1:]])
+        dataset.append(tmp)
 
-df_data = pd.DataFrame(data=dataset, columns=header)
-df_data.head()
+    df_data = pd.DataFrame(data=dataset, columns=header)
+    df_data.head()
 
-# Generate dataset.
-frequencies_modes_list = []
-parameters_list = []
-images = []
+    # Generate dataset.
+    frequencies_modes_list = []
+    parameters_list = []
+    images = []
 
-for i in tqdm.tqdm(range(len(struct))):
-    # Parameters.
-    sample_id = int(struct[i][0])
-    parameters = df_data[df_data['id'] == sample_id].values[0][1:]
-    parameters_list.append(parameters)
+    for i in tqdm.tqdm(range(len(struct))):
+        # Parameters.
+        sample_id = int(struct[i][0])
+        parameters = df_data[df_data['id'] == sample_id].values[0][1:]
+        parameters_list.append(parameters)
 
-    # Frequencies and modes.
-    freqs, modes = struct[i][2][:, 0].real, struct[i][2][:, 2].real
-    frequencies_modes_list.append((freqs, modes))
+        # Frequencies and modes.
+        freqs, modes = struct[i][2][:, 0].real, struct[i][2][:, 2].real
+        frequencies_modes_list.append((freqs, modes))
 
-    # Images.
-    omega_total, delta_omega_total, D1_total, D2_total = preproc(freqs, modes)
-    fig = plt.figure(figsize=(2, 1))
-    fig.add_subplot(111)
-    plt.xlim((130, 430))
-    plt.ylim((-500, 500))
-    plt.axis('off')
-    img = plt.scatter(omega_total * 1e-12, delta_omega_total * 1e-9, s=0.01)
-    fig.canvas.draw()
+        # Images.
+        omega_total, delta_omega_total, D1_total, D2_total = preproc(freqs, modes)
+        fig = plt.figure(figsize=(2, 1))
+        fig.add_subplot(111)
+        plt.xlim((130, 430))
+        plt.ylim((-500, 500))
+        plt.axis('off')
+        img = plt.scatter(omega_total * 1e-12, delta_omega_total * 1e-9, s=0.01)
+        fig.canvas.draw()
 
-    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-    data = data[5:-5, 20:-13, 0]
-    data = (data < 200) * 255
-    data = data.astype('uint8')
-    images.append(data)
-    plt.close()
+        data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+        data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+        data = data[5:-5, 20:-13, 0]
+        data = (data < 200) * 255
+        data = data.astype('uint8')
+        images.append(data)
+        plt.close()
 
-# Convert to np.array.
-images = np.array(images)
-frequencies_modes_list = np.array(frequencies_modes_list)
-parameters_list = np.array(parameters_list)
+    # Convert to np.array.
+    images = np.array(images)
+    frequencies_modes_list = np.array(frequencies_modes_list)
+    parameters_list = np.array(parameters_list)
 
-with open('dataset/1056-5-parameters.pkl', 'wb') as f:
-    pickle.dump(parameters_list, f)
+    with open('dataset/1056-5-parameters.pkl', 'wb') as f:
+        pickle.dump(parameters_list, f)
 
-with open('dataset/frequencies_modes.pkl', 'wb') as f:
-    pickle.dump(frequencies_modes_list, f)
+    with open('dataset/frequencies_modes.pkl', 'wb') as f:
+        pickle.dump(frequencies_modes_list, f)
 
-with open('dataset/1056-62-111-images.pkl', 'wb') as f:
-    pickle.dump(images, f)
+    with open('dataset/1056-62-111-images.pkl', 'wb') as f:
+        pickle.dump(images, f)
